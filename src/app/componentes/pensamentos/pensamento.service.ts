a494ec97faa98cb007be2b6d8379697826a604d2
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-09-27 18:35:49 -0300

    feat: Excluir Pensamento Component

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -7,17 +7,35 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PensamentoService {
-
+  // Define a URL da API
   private readonly API = "http://localhost:3000/pensamentos"
 
-
+// Construtor do serviço
   constructor(private http: HttpClient) { }
+
+  //Metodo de Listar Pensamentos
   listar():Observable<Pensamento[]>{
-    //Chama o método get do HttpClient para fazer uma requisição HTTP GET para a URL this.API.
+    //Chama o método get do HttpClient para fazer uma requisição HTTP GET para a URL this.API. em uma lista de Pensamentos
     return this.http.get<Pensamento[]>(this.API)
   }
+  //Metodo Criar um novo pensamento
   criar(pensamento:Pensamento):Observable<Pensamento> {
+    // Chama o método post do HttpClient para fazer uma requisição HTTP POST para a URL this.API, enviando o pensamento como corpo da requisição.
     return this.http.post<Pensamento>(this.API,pensamento)
+  }
+  //Metodo Exclui um pensamento
+  excluir(id:number):Observable<Pensamento>{
+    // Cria a URL para a requisição DELETE, direcionando o ID do pensamento.
+    const url = `${this.API}/${id}`
+    // Chama o método delete do HttpClient para fazer uma requisição HTTP DELETE para a URL criada.
+    return this.http.delete<Pensamento>(url)
+  }
+  //Metodo Buscar Por ID
+  buscarPorId(id:number):Observable<Pensamento>{
+     // Cria a URL para a requisição GET, incluindo o ID do pensamento.
+     const url =`${this.API}/${id}`
+     // Chama o método get do HttpClient para fazer uma requisição HTTP GET para a URL criada.
+     return this.http.get<Pensamento>(url);
 
   }
 

5cde30ded93462ff4a076b66f2d75802256b5bd3
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-10-30 23:49:00 -0300

    wip: Formulario reativo no editar

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,6 +1,7 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
-import { Pensamento } from './../pensamento';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,31 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  formulario!:FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario= this.formBuilder.group({
+      conteudo:['',Validators.compose(
+        [Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]
+
+      )],
+      autoria:['',Validators.compose(
+        [Validators.required,
+         Validators.minLength(3)
+        ])],
+      modelo:['',[Validators.required]]
+    })
+
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento
+      this.formulario.setValue(pensamento)
     })
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => {
+    this.service.editar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
-
   }
 
   cancelar() {
